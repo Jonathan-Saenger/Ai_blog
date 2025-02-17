2bfcedd76486c13924ddee319b50ecabf686a986
Jonathan Saenger <jonathan.saenger@outlook.fr>
2025-02-17 23:14:49 +0100

    MAIN > Article: Implémentation du template Article avec les boucles.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('partials/_article.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articleRepository->findBy([], ['createdAt' => 'DESC']),
         ]);
     }
 }
